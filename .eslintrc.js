ca09a182a4618d33fddfd4336c0426ed45f70ddf
Alexander Skripko <lifeisperfect3@gmail.com>
2024-04-01 09:01:55 +0300

    build: change eslint rules;

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,14 @@ module.exports = {
   },
   plugins: ['jest'],
   rules: {
-    'max-len': ['error', { ignoreComments: true }],
-    'import/prefer-default-export': 'off',
+    'no-console': 'off',
+    'no-alert': 'off',
+    'no-plusplus': 'off',
+    // '@stylistic/js/max-len': ['error', { code: 80 }, { ignoreComments: true }],
+    /* '@stylistic/js/newline-per-chained-call': [
+      'error',
+      { ignoreChainWithDepth: 2 },
+    ], */
+    'no-use-before-define': ['error', { functions: false }],
   },
 };
