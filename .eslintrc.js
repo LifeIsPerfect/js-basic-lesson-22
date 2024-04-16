8080d45ca4f318287d83f01559dc4379798b3a12
Alexander Skripko <lifeisperfect3@gmail.com>
2024-04-16 14:52:01 +0300

    style: remove commented code;

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,11 +25,6 @@ module.exports = {
     'no-console': 'off',
     'no-alert': 'off',
     'no-plusplus': 'off',
-    // '@stylistic/js/max-len': ['error', { code: 80 }, { ignoreComments: true }],
-    /* '@stylistic/js/newline-per-chained-call': [
-      'error',
-      { ignoreChainWithDepth: 2 },
-    ], */
     'no-use-before-define': ['error', { functions: false }],
   },
 };
