3a4dece0792dc3c67cd6dd477c584622337b10af
Alexander Skripko <lifeisperfect3@gmail.com>
2024-05-13 09:47:58 +0300

    feat: create chat page;

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 // prettier-ignore
 // eslint-disable-next-line no-unused-vars
-import css from "./style.css";
+// import css from "./style.css";
+import './style.css'
+import './chat/chat.css';
+import './chat/chat-title/chat-title.css';
+import './chat/chat-new-message-block/chat-new-message-block.css';
+import './chat/chat-messages/chat-messages.css';
+import './chat/user-or-chat-image/user-or-chat-image.css';
+import './chat/chat-message/chat-message.css';
 
 console.log('Test');
