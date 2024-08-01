2ca636cb9f2182e9d4278b053e950748be5cd0e1
Alexander Skripko <lifeisperfect3@gmail.com>
2024-08-01 09:51:52 +0300

    refactor: remove css styles duplication for blog;

--- src/blog/articles-list/al.js
+++ src/blog/articles-list/al.js
@@ -1,13 +1,13 @@
-import './al-page-header/al-page-header.css';
-import './al.css';
-import './al-article/al-article.css';
-import './al-tags-block/al-tags-block.css';
-import './al-main-menu/al-main-menu.css';
-import './al-page-footer/al-page-footer.css';
-import './al-site-icon/al-site-icon.css';
-import './al-main-layout/al-main-layout.css';
-import './al-navigation/al-navigation.css';
-import './al-adverts/al-adverts.css';
-import './al-adverts-item/al-adverts-item.css';
+import '../common/al-page-header/al-page-header.css';
+import '../common/common.css';
+import '../common/al-article/al-article.css';
+import '../common/al-tags-block/al-tags-block.css';
+import '../common/al-main-menu/al-main-menu.css';
+import '../common/al-page-footer/al-page-footer.css';
+import '../common/al-site-icon/al-site-icon.css';
+import '../common/al-main-layout/al-main-layout.css';
+import '../common/al-navigation/al-navigation.css';
+import '../common/al-adverts/al-adverts.css';
+import '../common/al-adverts-item/al-adverts-item.css';
 
 console.log('Articles list test');
